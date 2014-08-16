e30b6f7ec24ab05332c9a4461b6bc1e0d0e8ff5b
Satoshi Konno <skonno@cybergarage.org>
2014-08-16 16:49:45 +0900

    * Updated UPnP::CreateUUID() to use libuuid.

--- src/cybergarage/upnp/UPnP.cpp
+++ src/cybergarage/upnp/UPnP.cpp
@@ -24,6 +24,19 @@
 *
 ******************************************************************/
 
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#if defined(__APPLE__)
+#define HAVE_UUID_UUID_H 1
+#define HAVE_LIBUUID 1
+#endif
+
+#if defined(HAVE_UUID_UUID_H) || defined(HAVE_LIBUUID)
+#include <uuid/uuid.h>
+#endif
+
 #include <uhttp/net/HostInterface.h>
 #include <cybergarage/upnp/UPnP.h>
 #include <cybergarage/upnp/Icon.h>
@@ -63,15 +76,27 @@ const char *UPnP::GetServerName(std::string &buf) {
 ////////////////////////////////////////////////
 
 const char *UPnP::CreateUUID(std::string &buf) {
+#if defined(HAVE_LIBUUID)
+  #define CG_UPNP_UUID_MAX_LEN 128 /*(5 + ((4+1)*4) + 1)*/
+	uuid_t uuid;
+	char uuidStr[CG_UPNP_UUID_MAX_LEN];
+  uuid_generate(uuid);
+	uuid_unparse_lower(uuid, uuidStr);
+	char uuidBuf[CG_UPNP_UUID_MAX_LEN];
+	snprintf(uuidBuf,sizeof(uuidBuf), "%s",uuidStr);
+  buf = uuidStr;
+#else
   time_t time1 = time(NULL);
   time_t time2 = (time_t)((double)time(NULL) * ((double)rand() / (double)RAND_MAX));
   char sidBuf[((4+1)*4) + 1];
-  sprintf(sidBuf, "%04x-%04x-%04x-%04x", 
-    (int)(time1 & 0xFFFF), 
-    (int)(((time1 >> 31) | 0xA000) & 0xFFFF),
-    (int)(time2 & 0xFFFF), 
-    (int)(((time2 >> 31) | 0xE000) & 0xFFFF));
+  sprintf(sidBuf, "%04x-%04x-%04x-%04x",
+          (int)(time1 & 0xFFFF),
+          (int)(((time1 >> 31) | 0xA000) & 0xFFFF),
+          (int)(time2 & 0xFFFF),
+          (int)(((time2 >> 31) | 0xE000) & 0xFFFF));
   buf = sidBuf;
+#endif
+  
   return buf.c_str();
 }
 
