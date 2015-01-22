8c21aea33c098a836f5f8ed4c7ca8c6d201f5925
Satoshi Konno <skonno@cybergarage.org>
2015-01-22 21:51:39 +0900

    * Updated configure file for Homebrew.

--- pkg/homebrew/mupnp++.rb
+++ pkg/homebrew/mupnp++.rb
@@ -1,16 +1,20 @@
 class Mupnpxx < Formula
   homepage "http://www.cybergarage.org/do/view/Main/CyberLinkForCC"
-  url "https://github.com/cybergarage/mUPnPCC/archive/3.0.0.tar.gz"
-  sha1 "53c393abeecb5c449805f84c9a7b486a6fad8fee"
+  url "https://github.com/cybergarage/mUPnPCC/archive/3.0.1.tar.gz"
+  sha1 "78c8f6e105106447d456a89691791f15e5f28c93"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "readline"
   depends_on "uhttp++"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./bootstrap"
+    system "./configure_macosx", "--disable-debug",
+                                 "--disable-dependency-tracking",
+                                 "--disable-silent-rules",
+                                 "--prefix=#{prefix}"
+    system "make"
     system "make", "install"
   end
 end
