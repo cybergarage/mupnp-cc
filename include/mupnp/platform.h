c63912ee42fdf73ec3ef6af8166c36868f7d5c74
Satoshi Konno <skonno@cybergarage.org>
2015-01-30 00:29:25 +0900

    * Added Vector class.

---
+++ include/mupnp/platform.h
@@ -0,0 +1,32 @@
+/******************************************************************
+ *
+ * mUPnP for C++
+ *
+ * Copyright (C) Satoshi Konno 2002
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _MUPMPCC_PLATFORM_H_
+#define _MUPMPCC_PLATFORM_H_
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#if !defined(mupnp_shared_ptr)
+
+#if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201103L)) || defined(__APPLE__)
+#define HAVE_STD_SHRAEDPTR 1
+#define mupnp_shared_ptr std::shared_ptr
+#endif
+
+#if !defined(HAVE_STD_SHRAEDPTR)
+#include <boost/shared_ptr.hpp>
+#define mupnp_shared_ptr boost::shared_ptr
+#endif
+
+#endif
+
+#endif
